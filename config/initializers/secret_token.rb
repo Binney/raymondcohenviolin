a6021ad2230f09242fe31348f94cd4adcd9a13d8
SBinney <sar.binney@hotmail.co.uk>
2014-12-13 10:34:36 +0000

    Initialised repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Raymondcohenviolin::Application.config.secret_key_base = 'b3ef7e9d0a902a0d89462012bae0c49d9b7d6cccb74e93111f5d3d09daf423df290cbba413df9e1acf84b675f39e52e547f85ed251b77d353bb8b2094db6c03d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Raymondcohenviolin::Application.config.session_store :cookie_store, key: '_raymondcohenviolin_session'
