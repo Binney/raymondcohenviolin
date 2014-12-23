053294a8b6f67b03fb807c86f1604aa320a76b58
SBinney <sar.binney@hotmail.co.uk>
2014-12-23 13:33:07 +0000

    With s3 creds

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,10 @@
 class Picture < ActiveRecord::Base
-	has_attached_file :image # TODO ought to have a proper size/style hash here? 
+	has_attached_file :image,   # TODO ought to have a proper size/style hash here? 
+		storage: :s3,
+		path: ":attachment/:id/:style.:extension",
+  		bucket: "binneybucket",
+  		s3_credentials: "#{Rails.root}/config/s3.yml"
+
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
