2aa53bdb9121341dc02310f2fe231258db8c7c8d
SBinney <sar.binney@hotmail.co.uk>
2014-12-23 13:15:34 +0000

    Fix avatar into image

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
 	has_attached_file :image # TODO ought to have a proper size/style hash here? 
-    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
