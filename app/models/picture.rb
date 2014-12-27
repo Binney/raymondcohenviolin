e930d64c30eea6e225b3a9ca33871b42c1c9256d
SBinney <sar.binney@hotmail.co.uk>
2014-12-27 13:59:08 +0000

    More styling

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,13 +1,10 @@
 class Picture < ActiveRecord::Base
 	has_attached_file :image,   # TODO ought to have a proper size/style hash here? 
 		storage: :s3,
-		styles: {
-				thumb: '940x150>',
-				full: '2000x2000>'
-			},
+		styles: {thumb: '940x150>'},
 		path: ":attachment/:id/:style.:extension",
   		bucket: "binneybucket",
-  		s3_credentials: { bucket: ENV['S3_BUCKET_NAME'],
+  		s3_credentials: { bucket: "ENV['S3_BUCKET_NAME']",
       					  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       					  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] }
 
